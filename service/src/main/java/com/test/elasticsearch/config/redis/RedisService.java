b69e56d09ba4c12a21deaeeb467391741409d1b5
yemingrujing <yemingrujing@users.noreply.github.com>
2019-06-13 19:25:48 +0800

    redis

--- service/src/main/java/com/test/elasticsearch/config/redis/RedisService.java
+++ service/src/main/java/com/test/elasticsearch/config/redis/RedisService.java
@@ -596,14 +596,7 @@ public class RedisService {
      * @return
      */
     public static Long getCurrent2TodayEndMillisTime(Date date) {
-        Calendar todayEnd = Calendar.getInstance();
-        // Calendar.HOUR 12小时制
-        // HOUR_OF_DAY 24小时制
-        todayEnd.set(Calendar.HOUR_OF_DAY, 23);
-        todayEnd.set(Calendar.MINUTE, 59);
-        todayEnd.set(Calendar.SECOND, 59);
-        todayEnd.set(Calendar.MILLISECOND, 999);
-        return todayEnd.getTimeInMillis() - date.getTime();
+        return date.getTime() - System.currentTimeMillis();
     }
 
     public static void main(String[] args) {
